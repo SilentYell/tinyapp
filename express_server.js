f72cfb0f416aebc52188c25f4feb79b8bd8ab1d9
Nik Hixson <nikilobe@gmail.com>
2024-11-03 13:34:44 -0800

    add route for /urls/:id

--- express_server.js
+++ express_server.js
@@ -13,6 +13,14 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+  res.render("urls_show", templateVars);
+});http://localhost:8080/urls/b2xVn2
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
