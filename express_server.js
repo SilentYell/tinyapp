c4095dee6308965e5a7fc1774b465e420cd7bccc
Nik Hixson <nikilobe@gmail.com>
2024-11-09 09:43:40 -0800

    Create login page and update header to display login/register links

--- express_server.js
+++ express_server.js
@@ -122,6 +122,14 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
+app.get("/login", (req, res) => {
+  const userId = req.cookies["user_id"];
+  const user = users[userId];
+  const templateVars = { user };
+  res.render("login", templateVars);
+});
+
+
 app.post("/login", (req, res) => {
   const userId = req.cookies["user_id"];
   res.cookie("user_id", userId);
